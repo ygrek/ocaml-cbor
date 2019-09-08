66e9aa0b29341e5b7c2ecee586198dc1334799ee
zapashcanon <leo@ndrs.fr>
2019-09-08 06:06:42 +0200

    switch to dune

--- test/test.ml
+++ test/test.ml
@@ -1,5 +1,4 @@
-
-type result = Decoded of Yojson.Basic.json | Diagnostic of string
+type result = Decoded of Yojson.Basic.t | Diagnostic of string
 
 type test = {
   cbor : string;
@@ -17,7 +16,7 @@ let of_hex s =
   let n = String.length s / 2 in
   let r = Bytes.create n in
   for i = 0 to pred n do
-    r.[i] <- Char.chr @@ int_of_string ("0x" ^ String.sub s (i*2) 2)
+    Bytes.set r i (Char.chr @@ int_of_string ("0x" ^ String.sub s (i*2) 2))
   done;
   Bytes.to_string r
 
@@ -33,14 +32,14 @@ let read file =
       let result =
         try
           Diagnostic (Util.to_string @@ List.assoc "diagnostic" a)
-        with Not_found -> 
+        with Not_found ->
           Decoded (List.assoc "decoded" a)
       in
       { cbor; roundtrip; result }
     | _ -> assert false
     end
 
-let rec json_of_cbor : CBOR.Simple.t -> Yojson.Basic.json = function
+let rec json_of_cbor : CBOR.Simple.t -> Yojson.Basic.t = function
 | (`Null | `Bool _ | `Int _ | `Float _ as x) -> x
 | `Undefined | `Simple _ -> `Null
 | `Bytes x -> `String x
@@ -52,37 +51,33 @@ let rec json_of_cbor : CBOR.Simple.t -> Yojson.Basic.json = function
   | _ -> fail "json_of_cbor: expected string key") x)
 
 let () =
-  match List.tl @@ Array.to_list Sys.argv with
-  | file::[] ->
-    eprintfn "I: running tests from %s" file;
-    let tests = read file in
-    eprintfn "I: total tests = %d" (List.length tests);
-    let ok = ref 0 in
-    let failed = ref 0 in
-    let ignored = ref 0 in
-    let nr = ref (-1) in
-    tests |> List.iter begin fun test ->
-      try
-        incr nr;
-        let cbor = CBOR.Simple.decode test.cbor in
-        let diag = CBOR.Simple.to_diagnostic cbor in
-        let () = match test.result with
-        | Diagnostic s ->
-          if s <> diag then fail "expected %s, got %s" s diag
-        | Decoded json ->
-          let json' = json_of_cbor cbor in
-          if json <> json' then fail "expected %s, got %s, aka %s"
-            (Yojson.Basic.to_string json) (Yojson.Basic.to_string json') diag
-        in
-        incr ok
-      with exn ->
-        let ignore = List.mem !nr [10; 11; 12; 13; 47; 48; 49; 50; 51; 52; 71] in
-        eprintfn "%s test %d: %s"
-          (if ignore then "W: ignoring" else "E:") !nr (match exn with Failure s -> s | _ -> Printexc.to_string exn);
-        incr (if ignore then ignored else failed)
-    end;
-    eprintfn "I: finished. tests ok = %d failed = %d ignored = %d" !ok !failed !ignored;
-    exit (if !failed = 0 then 0 else 1)
-  | _ ->
-    eprintfn "E: no test file given";
-    exit 2
+  let file = "appendix_a.json" in
+  eprintfn "I: running tests from %s" file;
+  let tests = read file in
+  eprintfn "I: total tests = %d" (List.length tests);
+  let ok = ref 0 in
+  let failed = ref 0 in
+  let ignored = ref 0 in
+  let nr = ref (-1) in
+  tests |> List.iter begin fun test ->
+    try
+      incr nr;
+      let cbor = CBOR.Simple.decode test.cbor in
+      let diag = CBOR.Simple.to_diagnostic cbor in
+      let () = match test.result with
+      | Diagnostic s ->
+        if s <> diag then fail "expected %s, got %s" s diag
+      | Decoded json ->
+        let json' = json_of_cbor cbor in
+        if json <> json' then fail "expected %s, got %s, aka %s"
+          (Yojson.Basic.to_string json) (Yojson.Basic.to_string json') diag
+      in
+      incr ok
+    with exn ->
+      let ignore = List.mem !nr [10; 11; 12; 13; 47; 48; 49; 50; 51; 52; 71] in
+      eprintfn "%s test %d: %s"
+        (if ignore then "W: ignoring" else "E:") !nr (match exn with Failure s -> s | _ -> Printexc.to_string exn);
+      incr (if ignore then ignored else failed)
+  end;
+  eprintfn "I: finished. tests ok = %d failed = %d ignored = %d" !ok !failed !ignored;
+  exit (if !failed = 0 then 0 else 1)
